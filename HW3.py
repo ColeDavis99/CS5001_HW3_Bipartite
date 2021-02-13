b0b5d73b0e0422e7ef2da8f3b4ea46f6a64d2e5c
ColeDavis99 <coledavis6499@gmail.com>
2021-02-12 21:35:16 -0600

    Displaying graph properly now

--- HW3.py
+++ HW3.py
@@ -14,10 +14,18 @@ bg = nx.from_pandas_edgelist(matrix, source="name", target="title", edge_attr="u
 # Distinguish difference between movie node and critic node by making two sets (l set and r set)
 l, r = nx.bipartite.sets(bg)	#l is critics, r is movies
 
-#Assign position for each node (how it gets rendered with matplotlib)
+# Assign position for each node (how it gets rendered with matplotlib)
 pos = {}
 pos.update((node, (0,index)) for index, node in enumerate(l)) #Critics
 pos.update((node, (1,index)) for index, node in enumerate(r)) #Movies
 
-nx.draw(bg, pos=pos, with_labels=True)
+# Change color of node based on whether its a critic or movie
+color_map = []
+for node in bg:
+	if node in l:
+		color_map.append("red")
+	else:
+		color_map.append("blue")
+
+nx.draw(bg, pos=pos, node_color=color_map, with_labels=True)
 plt.show()
